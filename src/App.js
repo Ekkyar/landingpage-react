382fe65f7fc041e96cabfd6241efaf619160d93f
Ekky Aulia Rahman <Ekkyrahmanx1@gmail.com>
2022-10-16 01:51:45 +0700

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-white">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
